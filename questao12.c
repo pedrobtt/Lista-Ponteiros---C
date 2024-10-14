eb157defb0769cab9efaa8ee1efad258cf369cb3
pedrobtt <pedrobos21@gmail.com>
2024-10-14 08:06:41 -0300

    Add files via upload

---
+++ questao12.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main(){
+    int const *x = 3; // Isso é incorreto
+    printf("%d", ++(*x)); // Isso também causará erro
+    
+    /*
+        1º Erro: atribuir o valor 3 para um ponteiro. 
+        2º Erro: incrementar uma variável constante.
+    */
+}
+
