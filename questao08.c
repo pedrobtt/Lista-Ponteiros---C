2086308a9a51cab3ee36e4c331e665444084c18f
pedrobtt <pedrobos21@gmail.com>
2024-10-14 19:11:34 -0300

    Create questao08.c

---
+++ questao08.c
@@ -0,0 +1,8 @@
+/* 8. Assumindo que pulo[] é um vetor do tipo int, qual das seguintes expressões referenciam o valor
+do terceiro elemento do vetor? */
+
+*(pulo + 2); /* Correta: a expressão acesso o terceiro elemento do vetor, movendo duas posições para
+frente. Além disso o operador * acessa o valor referenciado */
+*(pulo + 4);
+pulo + 4;
+pulo + 2;
