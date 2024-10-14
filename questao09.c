9f8561b8751e87f252cfae3be0962f0d35b1e1c3
pedrobtt <pedrobos21@gmail.com>
2024-10-14 07:56:46 -0300

    Update questao09.c

--- questao09.c
+++ questao09.c
@@ -1,12 +1,16 @@
+/* 9. Considerando a declaração
+int mat[4], *p, x;
+quais das seguintes expressões são válidas? Justifique. */
+
 #include <stdio.h>
 
 int main(){
     int mat[4], *p, x;
     
-    p = mat + 1; // válido: aponta para o endereço do array + 1 (&mat[1])
-    p = mat; // válido: aponta para o endereço do array (&mat[0])
-    p = mat; // válido: aponta para o endereço do array (&mat[0])
-    x = (*mat); // válido: desreferencia o ponteiro e x retorna o valor de mat[0]
+    p = mat + 1; // Válido: aponta para o endereço do array + 1 (&mat[1])
+    p = mat; // Válido: aponta para o endereço do array (&mat[0])
+    p = mat; // Válido: aponta para o endereço do array (&mat[0])
+    x = (*mat); // Válido: desreferencia o ponteiro e x retorna o valor de mat[0]
 
     return 0;
 }
