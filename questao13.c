e19e90b45a61113fe6fefbd722d4e6168f18cfc0
pedrobtt <pedrobos21@gmail.com>
2024-10-14 08:05:46 -0300

    Update questao13.c

--- questao13.c
+++ questao13.c
@@ -1,3 +1,17 @@
+/* 13. Seja x um vetor de 4 elementos, declarado da forma TIPO x[4]. Suponha que depois da
+declaração, x esteja armazenado no endereço de memória 4092 (ou seja, o endereço de x[0]).
+Suponha também que na máquina seja usada uma variável do tipo char ocupa 1 byte, do tipo
+int ocupa 2 bytes, do tipo float ocupa 4 bytes e do tipo double ocupa 8 bytes. Quais serão os
+valores de x+1, x+2 e x+3 se:
+
+◦ x for declarado como char?
+◦ x for declarado como int?
+◦ x for declarado como float?
+◦ x for declarado como double?
+
+Implemente um programa de computador para testar estas suposições e compare as respostas
+oferecidas pelo programa com as respostas que você idealizou. */
+
 #include <stdio.h>
 
 int main() {
@@ -19,10 +33,8 @@ int main() {
     printf("Endereços de f: %p, %p, %p, %p\n", (void*)(f + 1), (void*)(f + 2), (void*)(f + 3)); 
     printf("Endereços de d: %p, %p, %p, %p\n", (void*)(d + 1), (void*)(d + 2), (void*)(d + 3)); 
     
-    /*
-    Linha 17 a 20: endereço das variáveis somado por 1, 2, 3 e multiplicado pela quantidade
-    de bytes em cada tipo de variável 
-    */
+    /* Linha 17 a 20: endereço das variáveis somado por 1, 2, 3 e multiplicado pela quantidade
+    de bytes em cada tipo de variável */
 
     return 0;
 }
