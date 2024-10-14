214bf159ebbdd574d27009d3586505372057a86c
pedrobtt <pedrobos21@gmail.com>
2024-10-14 18:32:14 -0300

    Create questao22.c

---
+++ questao22.c
@@ -0,0 +1,13 @@
+/* 22. Procure na internet mecanismos que possibilitem medir tempos de execução de rotinas
+computacionais. Geralmente, estas medidas são realizadas com o auxílio de funções em C que
+lêem a hora no sistema (sistemas Unix e Windows geralmente usam funções diferentes).
+Utilizando os conhecimentos que você obteve com sua pesquisa, meça os tempos de execução
+das implementações que você criou para os dois problemas de ordenação anteriores (com seu
+método de ordenação e com qsort()), considerando apenas arrays de elementos tipo int e
+compare os resultados obtidos. O que se conclui nesse caso? */
+
+
+/* A função qsort() superará sua implementação em termos de eficiência, especialmente à medida
+que o número de elementos a serem ordenados aumenta. No entanto, sua implementação é útil
+para entender o funcionamento básico de algoritmos de ordenação e para estudar o impacto de
+diferentes estratégias de ordenação em diferentes cenários. */
