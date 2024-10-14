9ed06fc0ee843d4ee4531846575eff915c223e07
pedrobtt <pedrobos21@gmail.com>
2024-10-14 08:18:39 -0300

    Update questao18.c

--- questao18.c
+++ questao18.c
@@ -1,3 +1,7 @@
+/* 18. O que é um ponteiro para uma função? Pesquise na Internet referências sobre o assunto e
+escreva um pequeno programa exemplificando o uso deste recurso. Explique seu programa,
+comentando cada uma das linhas de código. */
+
 #include <stdio.h>
 
 // Definindo uma função que soma dois inteiros
