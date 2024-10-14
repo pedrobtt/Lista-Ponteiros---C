1bebc4275141627a92f31a398be8dbfd914ebb03
pedrobtt <pedrobos21@gmail.com>
2024-10-14 07:49:20 -0300

    Update questao03.c

--- questao03.c
+++ questao03.c
@@ -1,15 +1,18 @@
+/* 3. Se i e j são variáveis inteiras e p e q ponteiros para int, quais das seguintes expressões de
+atribuição produzem erro de compilação? Justifique. */
+
 #include <stdio.h>
 
 int main(){
     int i, j, *p, *q;
     
     p = &i;
-    *q = &j; // erro de compilação : ponteiro q incompatível ao endereço de j
+    *q = &j; // Erro de compilação : ponteiro q incompatível ao endereço de j
     p = &*&i;
-    i = (*&)j; // erro de compilação : sintaxe inválida (*&)
+    i = (*&)j; // Erro de compilação : sintaxe inválida (*&)
     i = *&j; 
     i = *&*&j;
-    q = *p; // erro de compilação : tipos incompatíveis
+    q = *p; // Erro de compilação : tipos incompatíveis
     i = (*p)++ + *q;
 
     return 0;
